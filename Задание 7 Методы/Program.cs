cd7443dd22deca1b04ec02fc38b802be974dec38
Вадим Семочкин <vadimsemochkin96@yandex.ru>
2021-11-30 22:16:15 +0300

    Добавьте файлы проекта.

---
+++ Задание 7 Методы/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_7_Методы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длинны сторон первого треугольника");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите длинны сторон второго треугольника");
+            double k = Convert.ToDouble(Console.ReadLine());
+            double l = Convert.ToDouble(Console.ReadLine());
+            double m = Convert.ToDouble(Console.ReadLine());
+
+            double s1, s2;
+            Square(a, b, c,k ,l ,m, out s1, out s2);
+  
+            Console.WriteLine("Площадь первого треугольника равна {0}", s1);
+            Console.WriteLine("Площадь второго треугольника равна {0}", s2);
+
+            if (s1>s2)
+            {
+                Console.WriteLine("Площадь первого треугольника больше");
+            }
+            else
+            {
+                if (s2 == s1)
+                {
+                    Console.WriteLine("Площади треугольников равны");
+                }
+                else
+                {
+                    Console.WriteLine("Площадь второго треугольника больше");
+                }
+            }
+            Console.ReadKey();
+        }
+        static void Square(double a, double b, double c, double k, double l, double m, out double s1, out double s2)
+        {
+             double p1 = (a + b + c) / 2;
+             s1 = Math.Sqrt((p1 * (p1 - a) * (p1 - b) * (p1 - c)));
+             double p2 = (k + l + m) / 2;
+             s2 = Math.Sqrt((p2 * (p2 - k) * (p2 - l) * (p2 - m)));
+
+        }
+ 
+    }
+}
---
+++ Задача 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длинну стороны куба");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double V, S;
+            Volume(a, out V, out S);
+            Console.WriteLine("Объем куба равен {0}", V);
+            Console.WriteLine("Площадь поверхности куба равена {0}", S);
+            Console.ReadKey();
+        }
+        static void Volume(double a, out double V, out double S)
+        {
+            V = Math.Pow(a, 3);
+            S = 6*(a*a);
+
+        }
+    }
+ 
+}
